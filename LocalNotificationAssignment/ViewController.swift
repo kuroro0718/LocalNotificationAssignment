9362b8e9642491c68fe552e8d944ff3330988ae4
Alex Liang <u9211011@gmail.com>
2016-09-14 20:26:53 +0800

    Remove unused buttons and setup local notification after view did load

--- LocalNotificationAssignment/ViewController.swift
+++ LocalNotificationAssignment/ViewController.swift
@@ -9,50 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var timeTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let dateComponets = getDateComponets()
+        dateComponets.year = 2016
+        dateComponets.hour = 20
+        dateComponets.minute = 25
+        
+        let calendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)
+        let localNotification = UILocalNotification()
+        
+        // Push notification at 3 AM
+        localNotification.fireDate = calendar?.dateFromComponents(dateComponets)
+        localNotification.alertBody = "現在時間： 凌晨三點，來首好聽的歌曲吧"
+        localNotification.soundName = "3oclock.caf"
+        
+        UIApplication.sharedApplication().scheduleLocalNotification(localNotification)
+        
+        let transferNotification = UILocalNotification()
+        dateComponets.day = 1
+        // Push notification 1st day of month
+        transferNotification.fireDate = calendar?.dateFromComponents(dateComponets)
+        transferNotification.alertBody = "記得匯錢給Peter"
+        transferNotification.soundName = "3oclock.caf"
+        
+        UIApplication.sharedApplication().scheduleLocalNotification(transferNotification)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-
-    @IBAction func playMusicButtonPressed(sender: AnyObject) {
-        let dateComponets = getDateComponets()
-        
-        if dateComponets.hour == 21 {
-            let localNotification = UILocalNotification()
-            // Push notification at 3:30 AM
-            localNotification.fireDate = NSDate()
-            localNotification.alertBody = "現在時間： 凌晨三點，來首好聽的歌曲吧"
-            localNotification.soundName = "3oclock.caf"
-            
-            UIApplication.sharedApplication().scheduleLocalNotification(localNotification)
-        }
-    }
-    
-    @IBAction func transferButtonPressed(sender: AnyObject) {
-        let dateComponets = getDateComponets()
-        
-        if dateComponets.day == 1 {
-            let localNotification = UILocalNotification()
-            // Push notification 1st day of month
-            localNotification.fireDate = NSDate()
-            localNotification.alertBody = "記得匯錢給Peter"
-            localNotification.soundName = "3oclock.caf"
-            
-            UIApplication.sharedApplication().scheduleLocalNotification(localNotification)
-        }
     }
     
     func getDateComponets() -> NSDateComponents {
         let currentDate = NSDate()
         let calender = NSCalendar.currentCalendar()
         
-        return calender.components([.Month, .Day, .Hour], fromDate: currentDate)
+        return calender.components([.Month, .Day, .Hour, .Minute], fromDate: currentDate)
     }
 }
 
